575b973887f9293fd6fc604c1d90437180174716
Heloise Marconnet <hmarconn@z2r4p2.42lyon.fr>
2023-02-08 14:01:25 +0100

    norme

--- srcs/pa_utils.c
+++ srcs/pa_utils.c
@@ -12,27 +12,6 @@
 
 #include "../incs/cube.h"
 
-void	toparse_scroll(t_data *data, char *buffer)
-{
-	while (buffer[data->scroller] && buffer[data->scroller] != '\0' \
-		&& (buffer[data->scroller] < 33 || buffer[data->scroller] > 126))
-		data->scroller++;
-}
-
-int	pa_getlen(t_data *data, char *buffer)
-{
-	int	len;
-
-	len = 0;
-	while (buffer[data->scroller] && (buffer[data->scroller] >= 33
-			&& buffer[data->scroller] <= 126))
-	{
-		len++;
-		data->scroller++;
-	}
-	return (len);
-}
-
 void	print_type(int type)
 {
 	if (type == 0)
