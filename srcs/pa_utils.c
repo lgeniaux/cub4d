0343b9a3ac70ba105b2d85f63aea71f13f3f6cf2
Heloise Marconnet <hmarconn@z2r4p2.42lyon.fr>
2023-02-08 13:59:22 +0100

    norme

--- srcs/pa_utils.c
+++ srcs/pa_utils.c
@@ -49,6 +49,48 @@ void	print_type(int type)
 		printf("Error\nMap not found\n");
 }
 
+void	free_execution(t_data *data)
+{
+	int	y;
+
+	y = 0;
+	while (y < 8)
+	{
+		if (data->info->texture[y] != NULL)
+			free(data->info->texture[y]);
+		y++;
+	}
+	if (data->info->texture != NULL)
+		free(data->info->texture);
+	if (data->info->mlx)
+		free(data->info->mlx);
+	if (data->info != NULL)
+	{
+		free(data->info);
+	}
+	if (data->raycast != NULL)
+	{
+		free(data->raycast);
+	}
+}
+
+void	free_map(t_data	*data)
+{
+	int y;
+
+	y = 0;
+	if (data->map_exists == 1)
+	{
+		while (y < data->index)
+		{
+			if (data->mapper->map[y])
+				free(data->mapper->map[y]);
+			y++;
+		}
+		free(data->mapper->map);
+	}
+}
+
 void	the_end(t_data *data, int type)
 {
 	int	y;
@@ -57,21 +99,8 @@ void	the_end(t_data *data, int type)
 	print_type(type);
 	printf("fin\n");
 	if (data->buffer != NULL)
-	{
 		free(data->buffer);
-	}
-	if (data->map_exists == 1)
-	{
-		while (y < data->index)
-		{
-			if (data->mapper->map[y])
-			{
-				free(data->mapper->map[y]);
-			}
-			y++;
-		}
-		free(data->mapper->map);
-	}
+	free_map(data);
 	y = 0;
 	while (y < 4)
 	{
@@ -82,26 +111,6 @@ void	the_end(t_data *data, int type)
 	if (data->mapper->walls)
 		free(data->mapper->walls);
 	if (type == 6)
-	{
-		y = 0;
-		while (y < 8)
-		{
-			if (data->info->texture[y] != NULL)
-				free(data->info->texture[y]);
-			y++;
-		}
-		if (data->info->texture != NULL)
-			free(data->info->texture);
-		if (data->info->mlx)
-			free(data->info->mlx);
-		if (data->info != NULL)
-		{
-			free(data->info);
-		}
-		if (data->raycast != NULL)
-		{
-			free(data->raycast);
-		}
-	}
+		free_execution(data);
 	exit(18);
 }
