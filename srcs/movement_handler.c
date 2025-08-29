12540bf8b349e5928e01ad1813a01f84b9c7d427
lgeniaux <lgeniaux17@gmail.com>
2025-08-29 23:38:37 +0200

    Update old broken dependecies and QWERTY controls

--- srcs/movement_handler.c
+++ srcs/movement_handler.c
@@ -54,17 +54,15 @@ int	key_press(int key, t_data *data)
 		move_forward(data);
 	if (key == K_S || key == K_DOWN)
 		move_backward(data);
-	if (key == K_D)
-		rotate_right(data);
+	if (key == K_Q || key == K_LEFT)
+		move_left(data);
+	if (key == K_D || key == K_RIGHT)
+		move_right(data);
 	if (key == K_A)
 		rotate_left(data);
-	if (key == K_LEFT)
-		move_left(data);
-	if (key == K_RIGHT)
-		move_right(data);
+	if (key == K_E)
+		rotate_right(data);
 	if (key == K_ESC)
 		ft_exit(data);
-	mlx_clear_window(data->info->mlx, data->info->win);
-	main_loop(data);
 	return (0);
 }
